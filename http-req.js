8a8c700e55b2a7044a11a1542eb68fe8ccfcedc3
NoorulHuda2408 <noorulhuda2408@gmail.com>
2023-02-17 16:31:19 +0500

    HTTP Request Example

---
+++ http-req.js
@@ -0,0 +1,12 @@
+const { request, get } = require('http');
+const req = get('http://www.google.com',
+    (res) => {
+        res.on('data', (chunk) => {
+            console.log(`Data Chunk: ${chunk}`);
+        });
+
+        res.on('end', () => {
+            console.log('No more data');
+        });
+    })
+req.end();
